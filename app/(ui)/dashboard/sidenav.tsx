0e2ffcb7911508fa6aaf27b7d30f05aac360a48a
ayamba-coder <takuntohmartin@gmail.com>
2023-11-17 17:36:55 +0100

    added postres db

--- app/(ui)/dashboard/sidenav.tsx
+++ app/(ui)/dashboard/sidenav.tsx
@@ -22,7 +22,7 @@ export default function SideNav() {
       <div className="flex grow flex-row justify-between space-x-2  md:flex-col md:space-x-0 md:space-y-2 md:grow-0">
         <NavLinks />
         <form className="md:py-52 md:self-center">
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="w-full flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-red-500 text-white p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
