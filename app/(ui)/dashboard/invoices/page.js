07a97ac3f42fc425f9b050ae19c891cd16eff955
ayamba-coder <takuntohmartin@gmail.com>
2023-11-20 05:54:13 +0100

    more bettUI

--- app/(ui)/dashboard/invoices/page.js
+++ app/(ui)/dashboard/invoices/page.js
@@ -1,15 +1,3 @@
-import "@splidejs/react-splide/css";
-
-// or other themes
-import "@splidejs/react-splide/css/skyblue";
-import "@splidejs/react-splide/css/sea-green";
-
-// or only core styles
-import "@splidejs/react-splide/css/core";
-import { useState } from "react";
-//@ts-ignore
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-
 export default function Page() {
   return <div></div>;
 }
